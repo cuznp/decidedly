fea2e5aa131f2615db5a260f861c6a61d30a0239
punderwo <punderwo@gmail.com>
2014-07-18 19:13:04 -0700

    cleaned up some string value content

--- app/js/views/public/polls.js
+++ app/js/views/public/polls.js
@@ -47,7 +47,19 @@ define([
     		var id = $(event.target.parentElement).attr('id');
 
     		this.viewModel.goToPage('poll', id);
-    	}
+    	},
+
+        launchLogin: function () {
+            var thediv=document.getElementById('displaybox');
+            if(thediv.style.display == "none"){
+                thediv.style.display = "";
+                thediv.innerHTML = "<table width='100%' height='100%'><tr><td align='center' valign='middle' width='100%' height='100%'><object classid='clsid:02BF25D5-8C17-4B23-BC80-D3488ABDDC6B' codebase='http://www.apple.com/qtactivex/qtplugin.cab' width='640' height='500'><param name='src' value='http://cowcast.creativecow.net/after_effects/episodes/Shape_Tips_1_POD.mp4'><param name='bgcolor' value='#000000'><embed src='http://cowcast.creativecow.net/after_effects/episodes/Shape_Tips_1_POD.mp4' autoplay='true' pluginspage='http://www.apple.com/quicktime/download/' height='500' width='640' bgcolor='#000000'></embed></object><br><br><a href='#' onclick='return clicker();'>CLOSE WINDOW</a></td></tr></table>";
+            }else{
+                thediv.style.display = "none";
+                thediv.innerHTML = '';
+            }
+            return false;
+        }
     });
 });
         
